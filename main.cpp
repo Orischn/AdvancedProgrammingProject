57c7ccc34986a85c9c0ed4510149916e9354172e
OriSchn <orischn02@gmail.com>
2022-07-29 18:40:03 +0300

    removed the file functionallity from here

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include "file.hpp"
 #include "flower.hpp"
 
 using namespace std;
-
-double findMinDistance();
-int getFileLength(string path);
-Flower* getDataFromFile(string path);
 
 int main(int argc, char** argv) {
     const string cFilePath = "data/classified.csv";
@@ -16,52 +11,21 @@ int main(int argc, char** argv) {
     Flower* ucFlowers = getDataFromFile(ucFilePath);
     int cLength = getFileLength(cFilePath);
     int ucLength = getFileLength(ucFilePath);
+    cout<<"k="<<argv[1]<<endl<<endl;
+    cout<<"euclidian:"<<endl<<endl;
     for (int i = 0; i < ucLength; i++) {
         ucFlowers[i].classifyFlower(cFlowers, cLength, stoi(argv[1]), &Flower::euclidianDisTo);
         cout<<ucFlowers[i].getFlowerType()<<endl;
     }
-}
-
-/*
- * The function gets a string path to a file and returns its length.
- * Input: A path to a file.
- * Output: The length of the file (in lines).
- */
-int getFileLength(string path) {
-    ifstream input;
-    input.open(path);
-    string line;
-    int count = 0;
-    while(getline(input, line)) {
-        count++;
+    cout<<endl<<"manhattan:"<<endl<<endl;
+    for (int i = 0; i < ucLength; i++) {
+        ucFlowers[i].classifyFlower(cFlowers, cLength, stoi(argv[1]), &Flower::manhattanDisTo);
+        cout<<ucFlowers[i].getFlowerType()<<endl;
     }
-    input.close();
-    return count;
-}
-
-/*
- * The function takes all the data from the files with information about flowers,
- * and returns a pointer to an array that contains all that data in the format of a Flower.
- * Input: a path to a file.
- * Output: a pointer to an array with the data in the file in the format of a Flower.
- */
-Flower* getDataFromFile(string path) {
-    ifstream input;
-    string line;
-    int length = getFileLength(path);
-    Flower* flowers = new Flower[length];
-    input.open(path);
-    for (int i = 0; i < length; i++) {
-        getline(input, line);
-        flowers[i].setSepalWidth(stod(line.substr(0, line.find(','))));
-        line = line.substr(line.find(',') + 1, line.length());
-        flowers[i].setSepalLength(stod(line.substr(0, line.find(','))));
-        line = line.substr(line.find(',') + 1, line.length());
-        flowers[i].setPetalWidth(stod(line.substr(0, line.find(','))));
-        line = line.substr(line.find(',') + 1, line.length());
-        flowers[i].setPetalLength(stod(line.substr(0, line.find(','))));
-        line = line.substr(line.find(',') + 1, line.length());
-        flowers[i].setFlowerType(line);
+    cout<<endl<<"chebyshev:"<<endl<<endl;
+    for (int i = 0; i < ucLength; i++) {
+        ucFlowers[i].classifyFlower(cFlowers, cLength, stoi(argv[1]), &Flower::chebyshevDisTo);
+        cout<<ucFlowers[i].getFlowerType()<<endl;
     }
-    return flowers;
+    cout<<endl<<endl;
 }
